370cb219d45ddb10d2ed88d093b5f8aeeff1c1dc
josh9445 <josh_taylor@esri.com>
2021-03-21 08:06:20 -0700

    dynamic article linking

--- src/pages/ArticlesList.jsx
+++ src/pages/ArticlesList.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from "./article-content";
 
 const ArticlePage = () => {
 	return (
 		<>
 			<h1>Articles</h1>
+			{articleContent.map((article, key) => (
+				<Link className="article-list-item" to={`/article/${article.name}`} key={key}>
+					<h3>{article.title}</h3>
+					<p>{article.content[0].substring(0, 150)}...</p>
+				</Link>
+			))}
 		</>
 	);
 };
